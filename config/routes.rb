aa9ba4d01411ed2bf791e7f089a83a41e964a5e1
vagrant <vagrant@localhost.localdomain>
2015-09-20 05:42:58 +0000

    routing test.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'disco_boxes#index'
   resources :disco_boxes
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
