b1f093c4a316860f4bd69ddf9674d7443216a666
近松真成 <chikamatsu@chikamatsumanarinoMacBook-Pro.local>
2017-02-21 02:25:55 +0900

    404 page 作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,34 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  rescue_from Exception,                        with: :render_500
+  rescue_from ActiveRecord::RecordNotFound,     with: :render_404
+  rescue_from ActionController::RoutingError,   with: :render_404
+
+  def routing_error
+    raise ActionController::RoutingError.new(params[:path])
+  end
+
+  def render_404(e = nil)
+    logger.info "Rendering 404 with exception: #{e.message}" if e
+
+    if request.xhr?
+      render json: { error: '404 error' }, status: 404
+    else
+      format = params[:format] == :json ? :json : :html
+      render file: Rails.root.join('public/404.html.erb'), status: 404, layout: false, content_type: 'text/html'
+    end
+  end
+
+  def render_500(e = nil)
+    logger.info "Rendering 500 with exception: #{e.message}" if e 
+
+    if request.xhr?
+      render json: { error: '500 error' }, status: 500
+    else
+      format = params[:format] == :json ? :json : :html
+      render file: Rails.root.join('public/500.html'), status: 500, layout: false, content_type: 'text/html'
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
     resources :schedule
     resources :visual_art, only: [:index, :new, :create, :destroy]
   end
+  get '*path', controller: 'application', action: 'render_404'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
