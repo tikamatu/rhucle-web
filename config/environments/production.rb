aaa7bbb3756c3815054d64ba2f31626dbf2d4fc8
近松真成 <chikamatsu@chikamatsumanarinoMacBook-Pro.local>
2017-03-06 01:54:53 +0900

    addon sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,11 +82,12 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
+    :address => 'smtp.sendgrid.net',
     :port => 587,
     :user_name => ENV["auth_gmail_address"],
     :password => ENV["auth_gmail_paasword"],
     :authentication => :plain,
+    :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
 end
