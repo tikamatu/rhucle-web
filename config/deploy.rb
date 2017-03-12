11fc6cfaafc0a4fc6e5d2c1f0ab002f5803ad32b
近松真成 <chikamatsu@chikamatsumanarinoMacBook-Pro.local>
2017-03-12 20:29:10 +0900

    deploy settings

---
+++ config/deploy.rb
@@ -0,0 +1,40 @@
+# config valid only for current version of Capistrano
+lock "3.7.2"
+
+set :application, "rhucle-web"
+set :repo_url, "https://git.heroku.com/rhucle-web.git"
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/var/www/app/rhucle-web"
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+set :scm, :git
+set :log_level, :debug
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+set :bundle_binstubs, nil
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets bundle public/system public/assets}
+set :default_env, { path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH" }
+set :keep_releases, 5
+# Default value for :pty is false
+set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml", "config/secrets.yml"
+
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+namespace :deploy do
+  after :finishing, 'deploy:cleanup'
+end
